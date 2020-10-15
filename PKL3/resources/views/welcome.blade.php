d03fd9ef90014a954e687a60b7d42c98b8bd7a03
Dimas Ronal Kurniawan <61482999+Riltzthunder@users.noreply.github.com>
2020-10-15 22:06:51 +0700

    as

--- PKL3/resources/views/welcome.blade.php
+++ PKL3/resources/views/welcome.blade.php
@@ -9,12 +9,81 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-       <link rel="stylesheet" type="text/css" href="{{ asset('/css/navbar.css') }}">
-       <link rel="stylesheet" type="text/css" href="{{ asset('/css/sidebar.css') }}">
 
         <!-- Styles -->
         <style>
-            /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}.bg-white{--bg-opacity:1;background-color:#fff;background-color:rgba(255,255,255,var(--bg-opacity))}.bg-gray-100{--bg-opacity:1;background-color:#f7fafc;background-color:rgba(247,250,252,var(--bg-opacity))}.border-gray-200{--border-opacity:1;border-color:#edf2f7;border-color:rgba(237,242,247,var(--border-opacity))}.border-t{border-top-width:1px}.flex{display:flex}.grid{display:grid}.hidden{display:none}.items-center{align-items:center}.justify-center{justify-content:center}.font-semibold{font-weight:600}.h-5{height:1.25rem}.h-8{height:2rem}.h-16{height:4rem}.text-sm{font-size:.875rem}.text-lg{font-size:1.125rem}.leading-7{line-height:1.75rem}.mx-auto{margin-left:auto;margin-right:auto}.ml-1{margin-left:.25rem}.mt-2{margin-top:.5rem}.mr-2{margin-right:.5rem}.ml-2{margin-left:.5rem}.mt-4{margin-top:1rem}.ml-4{margin-left:1rem}.mt-8{margin-top:2rem}.ml-12{margin-left:3rem}.-mt-px{margin-top:-1px}.max-w-6xl{max-width:72rem}.min-h-screen{min-height:100vh}.overflow-hidden{overflow:hidden}.p-6{padding:1.5rem}.py-4{padding-top:1rem;padding-bottom:1rem}.px-6{padding-left:1.5rem;padding-right:1.5rem}.pt-8{padding-top:2rem}.fixed{position:fixed}.relative{position:relative}.top-0{top:0}.right-0{right:0}.shadow{box-shadow:0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06)}.text-center{text-align:center}.text-gray-200{--text-opacity:1;color:#edf2f7;color:rgba(237,242,247,var(--text-opacity))}.text-gray-300{--text-opacity:1;color:#e2e8f0;color:rgba(226,232,240,var(--text-opacity))}.text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.text-gray-500{--text-opacity:1;color:#a0aec0;color:rgba(160,174,192,var(--text-opacity))}.text-gray-600{--text-opacity:1;color:#718096;color:rgba(113,128,150,var(--text-opacity))}.text-gray-700{--text-opacity:1;color:#4a5568;color:rgba(74,85,104,var(--text-opacity))}.text-gray-900{--text-opacity:1;color:#1a202c;color:rgba(26,32,44,var(--text-opacity))}.underline{text-decoration:underline}.antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.w-5{width:1.25rem}.w-8{width:2rem}.w-auto{width:auto}.grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}@media (min-width:640px){.sm\:rounded-lg{border-radius:.5rem}.sm\:block{display:block}.sm\:items-center{align-items:center}.sm\:justify-start{justify-content:flex-start}.sm\:justify-between{justify-content:space-between}.sm\:h-20{height:5rem}.sm\:ml-0{margin-left:0}.sm\:px-6{padding-left:1.5rem;padding-right:1.5rem}.sm\:pt-0{padding-top:0}.sm\:text-left{text-align:left}.sm\:text-right{text-align:right}}@media (min-width:768px){.md\:border-t-0{border-top-width:0}.md\:border-l{border-left-width:1px}.md\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){.lg\:px-8{padding-left:2rem;padding-right:2rem}}@media (prefers-color-scheme:dark){.dark\:bg-gray-800{--bg-opacity:1;background-color:#2d3748;background-color:rgba(45,55,72,var(--bg-opacity))}.dark\:bg-gray-900{--bg-opacity:1;background-color:#1a202c;background-color:rgba(26,32,44,var(--bg-opacity))}.dark\:border-gray-700{--border-opacity:1;border-color:#4a5568;border-color:rgba(74,85,104,var(--border-opacity))}.dark\:text-white{--text-opacity:1;color:#fff;color:rgba(255,255,255,var(--text-opacity))}.dark\:text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}}
+            /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}
+            .bg-white{--bg-opacity:1;background-color:#fff;background-color:rgba(255,255,255,var(--bg-opacity))}
+            .bg-gray-100{--bg-opacity:1;background-color:#f7fafc;background-color:rgba(247,250,252,var(--bg-opacity))}
+            .border-gray-200{--border-opacity:1;border-color:#edf2f7;border-color:rgba(237,242,247,var(--border-opacity))}
+            .border-t{border-top-width:1px}
+            .flex{display:flex}
+            .grid{display:grid}
+            .hidden{display:none}
+            .items-center{align-items:center}
+            .justify-center{justify-content:center}
+            .font-semibold{font-weight:600}
+            .h-5{height:1.25rem}
+            .h-8{height:2rem}
+            .h-16{height:4rem}
+            .text-sm{font-size:.875rem}
+            .text-lg{font-size:1.125rem}
+            .leading-7{line-height:1.75rem}
+            .mx-auto{margin-left:auto;margin-right:auto}
+            .ml-1{margin-left:.25rem}
+            .mt-2{margin-top:.5rem}
+            .mr-2{margin-right:.5rem}
+            .ml-2{margin-left:.5rem}
+            .mt-4{margin-top:1rem}
+            .ml-4{margin-left:1rem}
+            .mt-8{margin-top:2rem}
+            .ml-12{margin-left:3rem}
+            .-mt-px{margin-top:-1px}
+            .max-w-6xl{max-width:72rem}
+            .min-h-screen{min-height:100vh}
+            .overflow-hidden{overflow:hidden}
+            .p-6{padding:1.5rem}
+            .py-4{padding-top:1rem;padding-bottom:1rem}
+            .px-6{padding-left:1.5rem;padding-right:1.5rem}
+            .pt-8{padding-top:2rem}
+            .fixed{position:fixed}
+            .relative{position:relative}
+            .top-0{top:0}
+            .right-0{right:0}
+            .shadow{box-shadow:0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06)}
+            .text-center{text-align:center}
+            .text-gray-200{--text-opacity:1;color:#edf2f7;color:rgba(237,242,247,var(--text-opacity))}
+            .text-gray-300{--text-opacity:1;color:#e2e8f0;color:rgba(226,232,240,var(--text-opacity))}
+            .text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}
+            .text-gray-500{--text-opacity:1;color:#a0aec0;color:rgba(160,174,192,var(--text-opacity))}
+            .text-gray-600{--text-opacity:1;color:#718096;color:rgba(113,128,150,var(--text-opacity))}
+            .text-gray-700{--text-opacity:1;color:#4a5568;color:rgba(74,85,104,var(--text-opacity))}
+            .text-gray-900{--text-opacity:1;color:#1a202c;color:rgba(26,32,44,var(--text-opacity))}
+            .underline{text-decoration:underline}
+            .antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}
+            .w-5{width:1.25rem}
+            .w-8{width:2rem}
+            .w-auto{width:auto}
+            .grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}@media (min-width:640px){
+            .sm\:rounded-lg{border-radius:.5rem}
+            .sm\:block{display:block}
+            .sm\:items-center{align-items:center}
+            .sm\:justify-start{justify-content:flex-start}
+            .sm\:justify-between{justify-content:space-between}
+            .sm\:h-20{height:5rem}
+            .sm\:ml-0{margin-left:0}
+            .sm\:px-6{padding-left:1.5rem;padding-right:1.5rem}
+            .sm\:pt-0{padding-top:0}
+            .sm\:text-left{text-align:left}.sm\:text-right{text-align:right}}@media (min-width:768px){
+            .md\:border-t-0{border-top-width:0}
+            .md\:border-l{border-left-width:1px}.md\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){
+                
+            .lg\:px-8{padding-left:2rem;padding-right:2rem}}@media (prefers-color-scheme:dark){
+            .dark\:bg-gray-800{--bg-opacity:1;background-color:#2d3748;background-color:rgba(45,55,72,var(--bg-opacity))}
+            .dark\:bg-gray-900{--bg-opacity:1;background-color:#1a202c;background-color:rgba(26,32,44,var(--bg-opacity))}
+            .dark\:border-gray-700{--border-opacity:1;border-color:#4a5568;border-color:rgba(74,85,104,var(--border-opacity))}
+            .dark\:text-white{--text-opacity:1;color:#fff;color:rgba(255,255,255,var(--text-opacity))}
+            .dark\:text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}}
         </style>
 
         <style>
@@ -67,15 +136,15 @@
   </div>
 </nav>
     
-  <div class="d-flex" id="wrapper">
+  <div class="d-flex" id="wrapper" style="height: auto; min-height:100%" >
 
      <!-- Sidebar -->
 
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
+  <div class="collapse navbar-collapse " id="navbarNavDropdown">
+        <ul class="navbar-nav">
       
       <li class="nav-item dropdown d-sm-block d-md-none">
-        <a class="nav-link dropdown-toggle" href="#" id="smallerscreenmenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle" href="#" id="smallerscreenmenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
           Menu
         </a>
         <div class="dropdown-menu" aria-labelledby="smallerscreenmenu">
@@ -89,32 +158,29 @@
 </nav>
 
 
-<div class="row" id="body-row">
+<aside class="row" id="body-row">
     <div id="sidebar-container" class="sidebar-expanded d-none d-md-block">
         <ul class="list-group">
-            <li class="list-group-item sidebar-separator-title text-muted d-flex align-items-center menu-collapsed">
+            <li class="list-group-item sidebar-separator-title text-white d-flex align-items-center menu-collapsed bg-dark">
                 <small>MAIN MENU</small>
             </li>
+
+
+            
             <a href="#submenu1" data-toggle="collapse" aria-expanded="false" class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
-                <div class="d-flex w-100 justify-content-start align-items-center">
+                <div class="d-flex w-100 justify-content-start align-items-center text-white">
                     <span class="fa fa-dashboard fa-fw mr-3"></span>
                     <span class="menu-collapsed">Dashboard</span>
                     <span class="submenu-icon ml-auto"></span>
                 </div>
             </a>
             <div id='submenu1' class="collapse sidebar-submenu">
-                <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                    <span class="menu-collapsed">Charts</span>
-                </a>
-                <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                    <span class="menu-collapsed">Reports</span>
-                </a>
-                <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                    <span class="menu-collapsed">Tables</span>
+               <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
+                    <span class="menu-collapsed">About</span>
                 </a>
             </div>
             <a href="#submenu2" data-toggle="collapse" aria-expanded="false" class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
-                <div class="d-flex w-100 justify-content-start align-items-center">
+                <div class="d-flex w-100 justify-content-start align-items-center text-white">
                     <span class="fa fa-user fa-fw mr-3"></span>
                     <span class="menu-collapsed">Profile</span>
                     <span class="submenu-icon ml-auto"></span>
@@ -122,26 +188,30 @@
             </a>
             <div id='submenu2' class="collapse sidebar-submenu">
                 <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                    <span class="menu-collapsed">Settings</span>
+                    <span class="menu-collapsed">Login</span>
                 </a>
                 <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                    <span class="menu-collapsed">Password</span>
+                    <span class="menu-collapsed">Logout</span>
                 </a>
             </div>            
            
         </ul>
-    </div> <!-- End Sidebar -->
+    </aside> <!-- End Sidebar -->
 
 
     <!-- MAIN -->
-    <div class="col">
 
-    </div>
 </div>
 
 <div class="wrapper" align="center">
     <!-- Sidebar Constructor -->
-        <h1>Dashboard</h1>
+        <h1><strong>Dashboard</strong></h1>
+        <p>
+       <button><a href="http://localhost/PKL3/resources/views/artikel/sop.php">SOP</a></button>
+
+       <button><a href="http://localhost/PKL3/resources/views/artikel/panduan.php">Panduan</a></button>
+
+       <button><a href="http://localhost/PKL3/resources/views/artikel/standard.php">Standard</a></button></p>
 
        <p>
        <button><a href="http://localhost/PKL3/resources/views/upload/HalamanUpload.blade.php">Untuk Upload Silahkan Disini</a></button></p>
