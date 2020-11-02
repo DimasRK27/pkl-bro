8be1254d24f2beb358634edd12ec0084cba6d479
Dimas Ronal Kurniawan <61482999+Riltzthunder@users.noreply.github.com>
2020-11-02 23:40:20 +0700

    as

---
+++ PKL3/app/Http/Controllers/Panduan.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Panduan extends Controller
+{
+      public function Panduan(){
+    	return view('artikel/panduan');
+    }
+}
